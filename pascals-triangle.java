163ef4f5c071bce500bb95a4efa9b3ceb2989810
magin <32916288+papamagin@users.noreply.github.com>
2023-10-16 13:57:19 +0300

    Create pascals-triangle.java

---
+++ pascals-triangle.java
@@ -0,0 +1,26 @@
+// https://leetcode.com/problems/pascals-triangle/description/
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (numRows == 0) {
+            return res;
+        }
+
+        List<Integer> firstRow = new ArrayList<>();
+        firstRow.add(1);
+        res.add(firstRow);
+
+        for (int i = 1; i < numRows; i++) {
+            List<Integer> prevRow = res.get(i - 1);
+            List<Integer> newRow = new ArrayList<>();
+            newRow.add(1);
+            for (int j = 1; j < i; j++) {
+                newRow.add(prevRow.get(j - 1) + prevRow.get(j));
+            }
+            newRow.add(1);
+            res.add(newRow);
+        }
+
+        return res;
+    }
+}
